6eeb1377b2e38066d4b38cb33032655696c450b4
Vishal Agarwal <36950862+Vishal260700@users.noreply.github.com>
2019-03-19 04:37:25 +0530

    Update game.js

--- games/FLAPPY-LEARNING/public/js/game.js
+++ games/FLAPPY-LEARNING/public/js/game.js
@@ -286,10 +286,10 @@ Game.prototype.display = function(){
 
 window.onload = function(){
 	var sprites = {
-		bird:"./FLAPPY-LEARNING/public/img/bird.png",
-		background:"./FLAPPY-LEARNING/public/img/background.png",
-		pipetop:"./FLAPPY-LEARNING/public/img/pipetop.png",
-		pipebottom:"./FLAPPY-LEARNING/public/img/pipebottom.png"
+		bird:"./public/img/bird.png",
+		background:"./public/img/background.png",
+		pipetop:"./public/img/pipetop.png",
+		pipebottom:"./public/img/pipebottom.png"
 	}
 
 	var start = function(){
